53d2b1abfa9e439a3b1e6ecb8705529d44d0fac9
Zeroexe00 <exe_6084@hotmail.com>
2019-09-17 20:39:51 -0300

    first commit

---
+++ src/components/Game.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="game-container" v-if="downloaded" />
+  <div class="placeholder" v-else>
+    Cargando ...
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'Game',
+  data() {
+    return {
+      downloaded: false
+    }
+  },
+  async mounted() {
+    const game = await import(/* webpackChunkName: "game" */ '@/game/game')
+    this.downloaded = true
+    this.$nextTick(() => game.launch())
+  }
+}
+</script>
+
+
+<style lang="scss" scoped>
+.placeholder {
+  font-size: 2rem;
+  font-family: 'Courier New', Courier, monospace;
+}
+</style>
